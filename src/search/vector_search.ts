07a72eb7e4a657ec284a10dcd9a638468ea7ef58
Kalamojo <kojo22444488888888@gmail.com>
2026-06-27 01:47:54 -0400

    Removing always checkout latest

--- src/search/vector_search.ts
+++ src/search/vector_search.ts
@@ -8,8 +8,6 @@ import logger from "../logger";
 const vectorSearch = async ( embedding: number[], table: lancedb.Table) => {
     const queryVector = normalize(embedding);
 
-    // Temporary; Todo: Implement more efficient read and write to tables
-    await table.checkoutLatest();
     let tableRes: CoverResult[] = await table.search(queryVector)
         .select(["cover_id", "book_id", "isbn_13", "cover_url", "_distance"])
         .limit(constants.vector_query_limit)
