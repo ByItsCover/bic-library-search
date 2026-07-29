aa9dd17b25c14c04264a251b03ea49593d9a118a
Kalamojo <kojo22444488888888@gmail.com>
2026-07-28 22:12:21 -0400

    Returning ratings for current user

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,8 @@ locals {
   api_gw_arn            = data.terraform_remote_state.bic_infra.outputs.api_gw_arn
   s3_db_uri             = data.terraform_remote_state.bic_infra.outputs.s3_db_uri
   hardcover_secret_name = data.terraform_remote_state.bic_infra.outputs.hardcover_secret_name
+  cognito_user_pool_id        = data.terraform_remote_state.bic_infra.outputs.auth_user_pool_id
+  cognito_user_pool_client_id = data.terraform_remote_state.bic_site.outputs.cognito_pool_client_id
   sqs_url               = data.terraform_remote_state.bic_infra.outputs.sqs_url
 }
 
@@ -27,6 +29,8 @@ resource "aws_lambda_function" "search_function" {
       ENVIRONMENT           = var.environment
       DB_URI                = local.s3_db_uri
       HARDCOVER_SECRET_NAME = local.hardcover_secret_name
+      COGNITO_USER_POOL_ID  = local.cognito_user_pool_id,
+      COGNITO_CLIENT_ID     = local.cognito_user_pool_client_id
       SQS_URL               = local.sqs_url
     }
   }
