819318ea27634bffb8f1dc3c6f57ceb4a6319c63
Kalamojo <kojo22444488888888@gmail.com>
2026-08-11 22:51:13 -0400

    Increasing memory and monitoring potential memory leak

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Router } from '@aws-lambda-powertools/event-handler/http';
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import * as ort from "onnxruntime-node";
 import logger from "./logger";
 import { health } from "./healthcheck/healthcheck";
 import search from "./search/search";
@@ -14,5 +15,7 @@ app.post('/search', [modelMiddleware, lanceMiddleware, customAuthMiddleware, har
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     logger.info(`Event: ${JSON.stringify(event, null, 2)}`);
     logger.info(`Context: ${JSON.stringify(context, null, 2)}`);
+    ort.env.logLevel = 'verbose';
+    ort.env.debug = true;
     return app.resolve(event, context);
 };
